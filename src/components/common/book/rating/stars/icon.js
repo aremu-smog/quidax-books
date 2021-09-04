af1cdd0ee2afd1fb22a412a4253c951f7fc684a3
Aremu Smog <aremuoluwagbamila@gmail.com>
2021-09-04 22:44:48 +0100

    feat: star rating system based on ratio

--- src/components/common/book/rating/stars/icon.js
+++ src/components/common/book/rating/stars/icon.js
@@ -1,28 +1,28 @@
-import React from "react";
+import React from "react"
 
 const StarIcon = ({ fill_ratio = 0.5 }) => {
+  const id = "fill" + Math.random() + Math.random()
+
+  const start = fill_ratio * 100
+  const end = start - 100
   return (
-    <svg
-      width="16"
-      height="15"
-      viewBox="0 0 16 15"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      {/* <defs>
-        <linearGradient id="half_grad">
-          <stop offset={fill_ratio} style={{ stopColor: "#EBA430" }} />
-          <stop offset={fill_ratio} style={{ stopColor: "#DDDDDD" }} />
+    <svg width='16' height='15' viewBox='0 0 16 15' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <defs>
+        <linearGradient id={id}>
+          <stop offset={"0%"} stopColor='#EBA430' />
+          <stop offset={`${start}%`} stopColor='#EBA430' />
+          <stop offset={`${start}%`} stopColor='#DDDDDD' />
+          <stop offset={`${end}%`} stopColor='#DDDDDD' />
         </linearGradient>
-      </defs> */}
+      </defs>
       <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M7.88382 0L10.332 4.93776L15.7676 5.72614L11.8257 9.56432L12.7593 15L7.88382 12.4481L3.0083 15L3.94191 9.56432L0 5.72614L5.45643 4.93776L7.88382 0Z"
-        fill={fill_ratio > 0 ? "#EBA430" : "#DDDDDD"}
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M7.88382 0L10.332 4.93776L15.7676 5.72614L11.8257 9.56432L12.7593 15L7.88382 12.4481L3.0083 15L3.94191 9.56432L0 5.72614L5.45643 4.93776L7.88382 0Z'
+        fill={`url(#${id})`}
       />
     </svg>
-  );
-};
+  )
+}
 
-export default StarIcon;
+export default StarIcon
--- src/components/common/book/rating/stars/index.js
+++ src/components/common/book/rating/stars/index.js
@@ -1,27 +1,27 @@
-import React from "react";
-import StarIcon from "./icon";
+import React from "react"
+import StarIcon from "./icon"
 
 const Stars = ({ no }) => {
-  const no_of_stars = parseInt(no);
-  const stars = [];
-  // const remainder = no - parseInt(no_of_stars);
-  // const upper_limit = Math.ceil(no);
+  const no_of_stars = no
+  const stars = []
+
+  const remainder = no_of_stars - parseInt(Math.floor(no_of_stars))
 
   for (let i = 1; i <= 5; i++) {
-    let fill_ratio = 0;
-    if (i <= no_of_stars) {
-      fill_ratio = 1;
-
-      // if (i === upper_limit && remainder !== 0) {
-      //   fill_ratio = remainder;
-      // }
+    let fill_ratio = 0
+    if (i < no_of_stars) {
+      fill_ratio = 1
+    } else if (i == Math.ceil(no_of_stars)) {
+      fill_ratio = remainder
+    } else {
+      fill_ratio = 0
     }
 
-    const star_icon = <StarIcon fill_ratio={fill_ratio} key={i} />;
-    stars.push(star_icon);
+    const star_icon = <StarIcon fill_ratio={fill_ratio} key={i} />
+    stars.push(star_icon)
   }
 
-  return <>{stars}</>;
-};
+  return <>{stars}</>
+}
 
-export default Stars;
+export default Stars
