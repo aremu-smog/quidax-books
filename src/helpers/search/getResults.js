c6e8eb0e122687c452847e522455adbd37cfc4fd
Aremu Smog <aremuoluwagbamila@gmail.com>
2021-09-06 15:52:46 +0100

    fix: search

--- src/helpers/search/getResults.js
+++ src/helpers/search/getResults.js
@@ -1,22 +1,18 @@
 import { getLongString } from "../string"
 export const getSearchResults = (books, searchTerm) => {
-  let searchResults = []
+  let searchResults = books.filter(book => {
+    if (searchTerm !== "") {
+      const wordRegex = new RegExp(searchTerm.toLowerCase(), "g")
 
-  setTimeout(() => {
-    searchResults = books.filter(book => {
-      if (searchTerm !== "") {
-        const wordRegex = new RegExp(searchTerm.toLowerCase(), "g")
+      const { title, genres, tags, authors } = book
 
-        const { title, genres, tags, authors } = book
+      const longString = getLongString(title, genres, tags, authors)
 
-        const longString = getLongString(title, genres, tags, authors)
-
-        return wordRegex.test(longString.toLowerCase())
-      } else {
-        return false
-      }
-    })
-  }, 300)
+      return wordRegex.test(longString.toLowerCase())
+    } else {
+      return false
+    }
+  })
 
   return searchResults
 }
